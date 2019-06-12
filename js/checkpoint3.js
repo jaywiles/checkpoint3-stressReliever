a4b0ac1506da817d6adb54a7f7aada3e4d512fee
jaywiles <jay.wiles@gmail.com>
2019-06-12 15:13:29 -0500

    lots of changes

--- js/checkpoint3.js
+++ js/checkpoint3.js
@@ -1,1 +1,39 @@
-console.log("test")
+// console.log("test")
+
+// two lines below link mug and pot images into JS from HTML
+// const mug = document.getElementById('#mug');
+// const pot = document.getElementById('#pot');
+// const pour = document.getElementById('#coffee-pour');
+
+// starting jQuery
+$(document).ready(function() {
+    $("#pot").click(function() {
+        $(this).animate({left: '-=75px'});
+        // $(this).animate()
+        $(this).rotate({bind:{
+            click: function(){
+                $(this).rotate({
+                    angle: 0,
+                    animateTo:-45,
+                    })
+                }
+            }
+        });
+    });
+
+});
+
+
+
+
+
+
+
+
+
+
+// function pourCoffee() {
+//     if {
+        
+//     }
+// }
