c7683e1141348cf3e3e01b61a7ba91814f27fe2f
jaywiles <jay.wiles@gmail.com>
2019-06-15 18:34:19 -0500

    changes

--- js/checkpoint3.js
+++ js/checkpoint3.js
@@ -6,49 +6,27 @@
 // const pour = document.getElementById('#coffee-pour');
 
 // starting jQuery
-// $(document).ready(function() {
-//     $("#pot").click(function() {
-//         $(this).animate({left: '-=125px'});
-//         // $(this).animate()
-//         $(this).rotate({bind:{
-//             click: function(){
-//                 $(this).rotate({
-//                     angle: 0,
-//                     animateTo:-45,
-                    
-//                     })
-//                 }
-//             }
-//         });
-//     });
-// });
-
-$(document).on("click", function(e) {
-    
-}
-
-
-function() {
-    $("#pot").bind("click", function() {
+$(document).ready(function() {
+    $("#pot").click(function() {
+        // below moves coffee pot to the left
         $(this).animate({left: '-=125px'});
-        // $(this).rotate(animateTo:-45,)
-        }})
-    })
-})
-
+        // below makes pot rotate
+        $(this).rotate({bind:{
+            click: function(){
+                $(this).rotate({
+                    angle: 0,
+                    animateTo:-45,
+                    })
+                }
+            }
+        })
+        // below makes sound effect play
+        $('audio#pour-sound')[0].play()
+    });
+});
 
 
 
 // audio sound effect below - automatically plays when page loads currently
 // $('audio#pour-sound')[0].play()
 
-
-
-
-
-
-// function pourCoffee() {
-//     if {
-        
-//     }
-// }
