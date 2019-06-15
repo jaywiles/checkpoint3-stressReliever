972977ab39d48fde25dec6da83403fb01589d567
jaywiles <jay.wiles@gmail.com>
2019-06-15 18:12:46 -0500

    add audio element

--- js/checkpoint3.js
+++ js/checkpoint3.js
@@ -6,26 +6,41 @@
 // const pour = document.getElementById('#coffee-pour');
 
 // starting jQuery
-$(document).ready(function() {
-    $("#pot").click(function() {
-        $(this).animate({left: '-=75px'});
-        // $(this).animate()
-        $(this).rotate({bind:{
-            click: function(){
-                $(this).rotate({
-                    angle: 0,
-                    animateTo:-45,
-                    })
-                }
-            }
-        });
-    });
+// $(document).ready(function() {
+//     $("#pot").click(function() {
+//         $(this).animate({left: '-=125px'});
+//         // $(this).animate()
+//         $(this).rotate({bind:{
+//             click: function(){
+//                 $(this).rotate({
+//                     angle: 0,
+//                     animateTo:-45,
+                    
+//                     })
+//                 }
+//             }
+//         });
+//     });
+// });
 
-});
+$(document).on("click", function(e) {
+    
+}
+
+
+function() {
+    $("#pot").bind("click", function() {
+        $(this).animate({left: '-=125px'});
+        // $(this).rotate(animateTo:-45,)
+        }})
+    })
+})
 
 
 
 
+// audio sound effect below - automatically plays when page loads currently
+// $('audio#pour-sound')[0].play()
 
 
 
